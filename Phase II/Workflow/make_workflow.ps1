256d2da163cdc4ec39d59017bce8589ac5fd87cf
Chad <chad.lowe@plus500.com>
2024-07-24 07:31:12 -0500

    Updated the workflow.

--- Phase II/Workflow/make_workflow.ps1
+++ Phase II/Workflow/make_workflow.ps1
@@ -1,7 +1,9 @@
 $yw = "D:\sandbox\CS513\yw-idcc-17\yw_jar\yesworkflow-0.2.0-jar-with-dependencies.jar"
 
-java -jar $yw graph menu_cleaning_outer_workflow.py -config graph.view=combined -config graph.layout=tb -config graph.portlayout=relax > .\menu_cleaning_outer_workflow.gv
+java -jar $yw graph menu_cleaning_outer_workflow-v2.py -config graph.view=combined -config graph.layout=tb -config graph.portlayout=relax > .\menu_cleaning_outer_workflow-v2.gv
 
-dot -Tpdf .\menu_cleaning_outer_workflow.gv -o menu_cleaning_outer_workflow.pdf
+dot -Tpdf .\menu_cleaning_outer_workflow-v2.gv -o menu_cleaning_outer_workflow-v2.pdf
 
-dot -Tsvg .\menu_cleaning_outer_workflow.gv -o menu_cleaning_outer_workflow.svg
+dot -Tsvg .\menu_cleaning_outer_workflow-v2.gv -o menu_cleaning_outer_workflow-v2.svg
+
+dot -Tpng .\menu_cleaning_outer_workflow-v2.gv -o menu_cleaning_outer_workflow-v2.png
---
+++ Phase II/Workflow/menu_cleaning_outer_workflow-v2.py
@@ -0,0 +1,73 @@
+# @begin CS512_Phase_II @desc Display one or more greetings to the user.
+# @in menu_csv @file file:menu.csv
+# @out cleaned_menu @as cleaned_menu @file file:CleanedMenu.csv
+# @out open_refine_history @as open_refine_history @file file:OpenRefineHistory.json
+
+    # @begin s1_profile_dataset_python @desc Python Dataset Profiling
+    # @in menu_csv @as menu_csv
+    # @out s1_dq_issues_python @as s1_dq_issues_python
+    # @end s1_profile_dataset_python
+
+    # @begin s1_profile_dataset_openrefine @desc OpenRefine Dataset Profiling
+    # @in menu_csv @as menu_csv
+    # @out s1_dq_issues_openrefine @as s1_dq_issues_openrefine
+    # @end s1_profile_dataset_openrefine
+
+    # @begin s1_combined_dq_issues @desc Combined DQ Issues
+    # @in s1_dq_issues_python
+    # @in s1_dq_issues_openrefine
+    # @out s1_dq_issues @as s1_dq_issues
+    # @end s1_profile_dataset
+
+
+
+    # @begin s2_resolve_missing_values @desc Remove or Impute Missing Values
+    # @in menu_csv @as menu_csv
+    # @in s1_dq_issues_python
+    # @out menu_s2_4 @as menu_s2_4 @file s2_Clean_Missing_Values.ipynb
+    # @end s2_resolve_missing_values
+
+    # @begin s2_clean_physical_description @desc Clean physical_descrption Column
+    # @in menu_csv
+    # @in s1_dq_issues
+    # @out s2_openrefine_2 @as s2_openrefine_2 @file s2_openrefine_2.json
+    # @end s2_process_physical_description
+
+    # @begin s2_clean_location_data @desc Clean location_data Column
+    # @in menu_csv
+    # @in s1_dq_issues
+    # @out s2_openrefine_3 @as s2_openrefine_3 @file s2_openrefine_3.json
+    # @end s2_clean_location_data
+
+    # @begin s2_standardize_event @desc Standardize event Column
+    # @in menu_csv
+    # @in s1_dq_issues
+    # @out s2_openrefine_1 @as s2_openrefine_1 @file s2_openrefine_1.json
+    # @end s2_standardize_event
+
+    # @begin s2_merge @desc Merge Open Refine Cleaning Tasks
+    # @in s2_openrefine_2
+    # @in s2_openrefine_3
+    # @in s2_openrefine_1
+    # @out s2_combined_openrefine @as s2_combined_openrefine @file file:combined_openrefine.json
+    # @end s2_merge
+
+    # @begin s2_clean_missing_values @desc Remove or Impute Missing Values
+    # @in menu_csv @as menu_csv
+    # @in menu_s2_4
+    # @out s2_menu_csv @as s2_menu_csv @file file:menu_data_1.csv
+    # @end s2_clean_missing_values
+
+    # @begin s2_s2_combined_openrefine_cleaning @desc Final Cleaning of Dataset
+    # @in s2_menu_csv
+    # @in s2_combined_openrefine
+    # @out cleaned_menu @as cleaned_menu @file file:CleanedMenu.csv
+    # @out open_refine_history @as open_refine_history @file file:OpenRefineHistory.json
+    # @end s2_s2_combined_openrefine_cleaning
+
+    # @begin s3_data_quality_check @desc Data Quality Check
+    # @in cleaned_menu
+    # @end s3_data_quality_check
+
+    
+# @end CS512_Phase_II
